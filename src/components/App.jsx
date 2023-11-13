99a0e8ff02285576b2daa25bb832f5dc2777bd59
Abdul Rahman <abdulrahmans0414@gmail.com>
2023-11-14 02:17:16 +0530

    delete notes.jsx and fix app.jsx and Note.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Header from './Header'
 import Footer from './Footer'
 import Note from './Note'
-import { notes } from '../notes'
+
 import CreateArea from './CreateArea'
 
 
@@ -10,14 +10,13 @@ export default function App() {
   return (
     <div>
       <Header />
-      <CreateArea />
-      {notes.map(noteItem => ( 
+      <CreateArea />   
         <Note
-          key={noteItem.key}
-          title={noteItem.title}
-          content={noteItem.content}
+          key={1}
+          title="Note title"
+          content="Note content"
         />
-      ))}
+
       <Footer />
     </div>
   )
