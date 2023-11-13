ccfa250087d780672d46e942a41a04bfb8b964bf
Abdul Rahman <abdulrahmans0414@gmail.com>
2023-11-13 12:36:11 +0530

    complete challenge Create a Note.jsx component to show a <div> element with a //<h1> for a title and a <p> for the content

---
+++ src/components/Note.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Note() {
+    return (
+        <div className='note'>
+            <h1>This is title </h1>
+            <p>This is a content</p>
+        </div>
+    )
+}
