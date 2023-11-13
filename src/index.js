33ca34e1f43ba2fb9fa386b962319ebc5d280166
Abdul Rahman <abdulrahmans0414@gmail.com>
2023-11-13 12:14:58 +0530

    complete Create a App.jsx component. challenge

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import  ReactDOM from 'react-dom';
+import App from './components/App';
 
 
-ReactDOM.render(<h1>Hello Everyone!</h1>,document.getElementById("root"))
+ReactDOM.render(<App />,document.getElementById("root"))
 
-//2. Create a App.jsx component.
 //3. Create a Header.jsx component that renders a <header> element
 //to show the Keeper App name in an <h1>.
 //4. Create a Footer.jsx component that renders a <footer> element
