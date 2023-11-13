9ea795ef5183aa7f07b65410e4a3ccfd3e44199d
Abdul Rahman <abdulrahmans0414@gmail.com>
2023-11-14 01:44:18 +0530

    add component CreateArea.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,12 +3,14 @@ import Header from './Header'
 import Footer from './Footer'
 import Note from './Note'
 import { notes } from '../notes'
+import CreateArea from './CreateArea'
 
 
 export default function App() {
   return (
     <div>
       <Header />
+      <CreateArea />
       {notes.map(noteItem => ( 
         <Note
           key={noteItem.key}
